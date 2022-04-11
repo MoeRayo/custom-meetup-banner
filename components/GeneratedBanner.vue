1cf9226652bc4134be76470f98841f1253aeb14d
MoeRayo <moronfolu.motunrayo1@gmail.com>
2022-04-11 23:38:00 +0100

    feat: addition of the generated banner functionality

---
+++ components/GeneratedBanner.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div>
+    <cld-image :public-id="publicId" >
+      <cld-transformation effect="blur:300"/>
+      <cld-transformation :overlay="{fontFamily: 'Berkshire Swash', fontSize: 40, fontWeight: 'bold', text: title}" color="#fff" opacity="90" crop="fit" width="400" background="#000" x="-80" effect="shadow" y="-60"/>
+      <cld-transformation :overlay="{fontFamily: 'Lustria', fontSize: 16, fontWeight: '', text: description}" color="#000" crop="fit" width="190" x="-200" y="35"/>
+      <cld-transformation :overlay="{fontFamily: 'Lustria', fontSize: 15, fontWeight: 'bold', text: `Speakers: ${speakers}`}" color="#fff" effect="shadow"  crop="fit" width="120" background="#000" x="200" y="130"/>
+    </cld-image>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: { 
+      type: String, 
+      required: true 
+    },
+    description: { 
+      type: String, 
+      required: true 
+    },
+    speakers: { 
+      type: String, 
+      required: true
+    },
+    publicId: { 
+      type: String, 
+      required: true
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,87 @@
 <template>
-  <div>
+  <div class="bg-washed-yellow vh-100">
+    <header class="tc pv3 pv3-ns">
+      <h1 class="mt2 mb0 baskerville i fw1 f2-ns f3 mid-gray ">Custom Meetup Banner Generator</h1>
+    </header>
+    <section class="ph5">
+      <h2 class="baskerville fw1 f4-ns f3">Select an image</h2>
+
+      <div v-for="banner in banners" :key="banner.id" class="fl-ns db tc pa1 grow aspect-ratio--4x6" :class="banner.id === imageId ? 'ba bw2 b--green ' : ''" @click="selectImage(banner.id, banner.publicId)">
+        <cld-image :public-id="banner.publicId" crop="scale" width="150" height="100"></cld-image>
+      </div>
+
+      <p v-if="formData.imageError" class="cb red fw3 i f7">Please select an image</p>
+
+      <div class="mv4 cb">
+        <div class="mb3 w-34">
+          <label class="mb1 ttc f7 db" for="title">Title</label>
+          <input id="title" v-model="formData.title" type="text" name="title" class="db w-100 pv3 ph2 br2 ba b--black-40 f7" required>
+        </div>
+        <div class="mb3 w-34">
+          <label class="mb1 ttc f7 db" for="event-detail">event Detail</label>
+          <textarea id="event-detail" v-model="formData.eventDetail" name="event-detail" class="db w-100 resize h3 ph2 pv3 f7 br2 ba b--black-40" required></textarea>
+        </div>
+        <div class=" w-34">
+          <label class="mb1 ttc f7 db" for="speakers">Speakers</label>
+          <textarea id="speakers" v-model="formData.speaker" name="speakers" class="db w-100 resize h3 ph2 pv3 f7 br2 ba b--black-40" required></textarea>
+          <p v-if="formData.error" class="mt2 mb4 red fw3 i f7">Please fill all required fileds</p>
+        </div>
+      </div>
+      <button class="f6 link dim br2 ph3 pv2 mb2 dib white bg-dark-green ba b--green" @click="handleSubmit">Generate Banner</button>
+
+      <div v-if="showCard" class="mt-10">
+        <GeneratedBanner
+          :title="formData.title"
+          :description="formData.eventDetail"
+          :speakers="formData.speaker"
+          :publicId="formData.publicId"
+        />
+      </div>
+    </section>
   </div>
+
 </template>
 
 <script>
-export default {}
+  import banners from "~/utils/banner.json";
+
+  export default {
+    data() {
+      return {
+        banners,
+        imageId: null,
+        showCard: false,
+        formData: {
+          publicId: null,
+          error: false,
+          imageError: false,
+          title: '',
+          eventDetail: '',
+          speaker: ''
+        }
+      }
+    },
+
+  methods: {
+    selectImage(imageId, publicId) {
+      this.imageId = imageId
+      this.formData.publicId = publicId
+      this.formData.error = false
+      this.showCard = false
+    },
+    handleSubmit() {
+      if (!this.imageId || (this.formData.title === ''|| this.formData.speaker === '')) {
+        this.formData.imageError = true
+      } else {
+        this.showCard = true 
+      }
+    },
+  }
+}
 </script>
+
+<style>
+.resize {
+  resize: vertical;
+}
+</style>
